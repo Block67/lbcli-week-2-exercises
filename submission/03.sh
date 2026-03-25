a741f1845a2c5842de0986ce2ebec7a972675728
boda <boda@github.com>
2026-03-25 14:05:04 +0100

    fix: use 101 blocks for 03.sh to ensure mature funds

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,8 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
-ADDR=$(bitcoin-cli -regtest getnewaddress "" bech32)
-bitcoin-cli -regtest generatetoaddress 1 "$ADDR" > /dev/null
-echo "$ADDR"
+address=$(bitcoin-cli -regtest getnewaddress "" bech32)
+# Generate 101 blocks to the address to make the coinbase output mature
+bitcoin-cli -regtest generatetoaddress 101 $address > /dev/null
+# Return only the Address
+echo $address
